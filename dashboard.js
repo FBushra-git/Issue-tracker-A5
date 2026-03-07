84322f9a05578b68edaa581046280f3c7178d8c6
FBushra-git <mafiaqueen734@gmai.com>
2026-03-08 01:30:56 +0600

    Search Issues logical implementation

--- dashboard.js
+++ dashboard.js
@@ -165,10 +165,23 @@ const showOpen = () => {
 
 
 
-const showClosed = () => {
+const showClosed=()=> {
     setActive("closedBtn");
 
     displayIssues(
-        allIssues.filter(i => i.status === "closed")
+        allIssues.filter(i=>i.status==="closed")
     );
+};
+
+
+// Search issues
+
+const searchIssues = () =>{
+    const text = document.getElementById("searchInput").value;
+    fetch(
+        `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`
+    )
+    .then(res => res.json())
+    .then(json => displayIssues(json.data));
+
 };
